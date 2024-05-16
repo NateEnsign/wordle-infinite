188ec7fb518699c71a57d5267bec901efe9d7632
Nate Ensign <dixienate@gmail.com>
2024-05-16 15:15:03 -0600

    game grid and keyboard set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
+import React, { useState, createContext } from "react";
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./Words";
+
+export const AppContext = createContext();
 
 function App() {
- 
+  const [board, setBoard] = useState(boardDefault);
 
   return (
-    <>
-      <h1>Welcome Mutha Effas</h1>
-    </>
-  )
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
