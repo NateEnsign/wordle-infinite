1085934c48e83de191812d7698220a8dca3e125a
Nate Ensign <dixienate@gmail.com>
2025-01-08 22:23:15 -0700

    added a button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,6 +64,9 @@ const Navbar = () => {
           <button>
             <h3>Login</h3>
           </button>
+          <button>
+            <h3>Test</h3>
+          </button>
       </div>
     </nav>
   );
