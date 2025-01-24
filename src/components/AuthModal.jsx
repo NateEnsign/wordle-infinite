78e8356c5db488315a9bdba7f7582263f912ebc7
Nate Ensign <dixienate@gmail.com>
2025-01-23 17:48:02 -0700

    login modal has been completely built out and styled

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,83 +1,83 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import Modal from "react-modal";
 import { X } from "lucide-react";
 import { AppContext } from "../App";
 
 import './AuthModal.css';
 
-
-
 Modal.setAppElement("#root");
 
 const AuthModal = ({ authModalOpen, closeAuthModal }) => {
-  const { highContrast, darkMode } = useContext(AppContext)
+  const { highContrast, darkMode } = useContext(AppContext);
+
+  // State to toggle between login and signup modes
+  const [isLoginMode, setIsLoginMode] = useState(true);
 
   const correctLetter = !highContrast && !darkMode ? "how-letter-green-light" : !highContrast ? "how-letter-green-dark" : "how-letter-orange";
 
-  const almostLetter = !highContrast && !darkMode ? "how-letter-yellow-light" : !highContrast ? "how-letter-yellow-dark" : "how-letter-blue"
+  const almostLetter = !highContrast && !darkMode ? "how-letter-yellow-light" : !highContrast ? "how-letter-yellow-dark" : "how-letter-blue";
 
-  const nonLetter = !darkMode ? "how-letter-grey-light" : "how-letter-grey-dark"
+  const nonLetter = !darkMode ? "how-letter-grey-light" : "how-letter-grey-dark";
 
-  const customStyles = darkMode ? {
-    overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.55)",
-      overflow: "hidden",
-      zIndex: '1080',
-    },
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      backgroundColor: "#121214",
-      color: "#F8F8F8",
-      border: "none",
-      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-      borderRadius: "8px",
-      padding: "20px",
-      height: "550px",
-      width: "470px",
-      maxWidth: "80%",
-      maxHeight: "85%",
-      overflow: "auto",
-      // zIndex: '1080',
-    },
-  } : {
-    overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.55)",
-      overflow: "hidden",
-      zIndex: '1080',
-    },
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      backgroundColor: "white",
-      color: "#010101",
-      border: "none",
-      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-      borderRadius: "8px",
-      padding: "20px",
-      height: "550px",
-      width: "470px",
-      maxWidth: "80%",
-      maxHeight: "85%",
-      overflow: "auto",
-      // zIndex: '1080',
-    },
-  }
+  const customStyles = darkMode
+    ? {
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.55)",
+          overflow: "hidden",
+          zIndex: "1080",
+        },
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          backgroundColor: "#121214",
+          color: "#F8F8F8",
+          border: "none",
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+          borderRadius: "8px",
+          padding: "20px",
+          height: "550px",
+          width: "470px",
+          maxWidth: "80%",
+          maxHeight: "85%",
+          overflow: "auto",
+        },
+      }
+    : {
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.55)",
+          overflow: "hidden",
+          zIndex: "1080",
+        },
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          backgroundColor: "white",
+          color: "#010101",
+          border: "none",
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+          borderRadius: "8px",
+          padding: "20px",
+          height: "550px",
+          width: "470px",
+          maxWidth: "80%",
+          maxHeight: "85%",
+          overflow: "auto",
+        },
+      };
 
   return (
     <Modal
       isOpen={authModalOpen}
       onRequestClose={closeAuthModal}
       style={customStyles}
-    
     >
       <div className="auth-modal-body">
         <div className="auth-btn-container">
@@ -85,14 +85,25 @@ const AuthModal = ({ authModalOpen, closeAuthModal }) => {
             <X className="auth-x" />
           </button>
         </div>
-       <div className="auth-form-body">
-        <h4>Login or Sign Up to track your stats!</h4>
-        <form className="auth-form-inputs">
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Password" />
-        </form>
-       </div>
+        <div className={!darkMode ? 'auth-form-body' : 'auth-form-body-dark'}>
+          <h3 className="auth-form-header">Must be logged in to track stats!</h3>
+          <form className='auth-form-inputs'>
+            {!isLoginMode && <input type="text" placeholder="Name" className={!darkMode ? 'auth-input' : 'auth-input-dark'} />}
+            <input type="email" placeholder="Email" className={!darkMode ? 'auth-input' : 'auth-input-dark'} />
+            <input type="password" placeholder="Password" className={!darkMode ? 'auth-input' : 'auth-input-dark'}  />
+            <button className={darkMode ? 'login-btn-dark' : 'login-btn-light'}>
+                {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+            </button>
+          </form>
+          <button
+            className={!darkMode ? 'toggle-mode-btn' : 'toggle-mode-btn-dark'}
+            onClick={() => setIsLoginMode(!isLoginMode)}
+          >
+            {isLoginMode
+              ? "Don't have an account? Sign Up"
+              : "Already have an account? Login"}
+          </button>
+        </div>
       </div>
     </Modal>
   );
